99582f9475dd9cf36a1ef4814833a9848c85ebfe
Michael <michael.sarrazin@protonmail.com>
2018-04-01 00:06:15 +0200

    nouvelle modif

--- content/posts/my-first-post.md
+++
@@ -1,6 +0,0 @@
----
-title: "My First Post"
-date: 2018-03-31T23:20:27+02:00
-draft: true
----
-This is the POSTTTTT.....
--- themes/simplicity/src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import '../styles/styles'
-
-import baguetteBox from 'baguettebox.js'
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  baguetteBox.run('main', {})
-})
